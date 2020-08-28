193c3ad0858214b9a6729a8e35af94cb93b051bd
Bastien Decorte <seriousbooster@gmail.com>
2020-08-28 15:16:12 +0200

    init git

---
+++ uniswap_script.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Uniswap coin warning remover
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  
+// @author       github.com/bo0st3r
+// @match        https://app.uniswap.org/
+// @grant        none
+// ==/UserScript==
+
+(async function() {
+    'use strict';
+
+    function wait(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    let btn, i=0;
+
+    // Loops every 200ms until the button is found (because it is displayed after page has loaded)
+    // If no button is found after 30 tries, stops
+    while(!btn && i++ < 30){
+        await wait(200).then(() => {
+            btn = document.querySelector('.fnFztX');
+        });
+    }
+
+    // Queries the "I understand" box
+    let checkBoxParent = document.querySelector('.understand-checkbox').parentElement;
+
+    // Checks the "I understand" box, then clicks on the button
+    if(btn && checkBoxParent){
+        checkBoxParent.click();
+        btn.click();
+    }
+})();
