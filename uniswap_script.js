00d6441d0cabb21e9f602f8179dbbdfab14ff930
Bastien Decorte <seriousbooster@gmail.com>
2021-01-23 11:24:15 +0100

    fix: update to work with Uniswap's new UI

--- uniswap_script.js
+++ uniswap_script.js
@@ -15,22 +15,20 @@
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
-    var btn, i=0;
-
-    // Loops every 200ms until the button is found (because it is displayed after page has loaded)
-    // If no button is found after 30 tries, stops
-    while(!btn && i++ < 100){
+    let understandBtn, tokenDismissBtn, i=0;
+    // Loops until buttons are found or reached max trials
+    while(!understandBtn && !tokenDismissBtn && i++ < 30){
         await wait(200).then(() => {
-            btn = document.querySelector('.token-dismiss-button');
+            console.log('try');
+            understandBtn = document.querySelector('.\\.understand-checkbox');
+            console.log(understandBtn);
+            tokenDismissBtn = document.querySelector('.\\.token-dismiss-button');
         });
     }
 
-    // Queries the "I understand" box
-    let checkBoxParent = document.querySelector('.understand-checkbox').parentElement;
-
-    // Checks the "I understand" box, then clicks on the button
-    if(btn && checkBoxParent){
-        checkBoxParent.click();
-        btn.click();
+    // Click buttons
+    if(understandBtn && tokenDismissBtn){
+        understandBtn.click();
+        tokenDismissBtn.click();
     }
 })();
