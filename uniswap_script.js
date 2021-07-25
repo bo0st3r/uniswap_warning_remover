1fe18887d885f6fa89c5fdf76263601d99d3c253
Bastien Decorte <seriousbooster@gmail.com>
2021-07-25 16:43:53 -0400

    update warning remover for Uniswap V3 + support Pancakeswap

--- uniswap_script.js
+++ uniswap_script.js
@@ -1,34 +1,39 @@
 // ==UserScript==
 // @name         Uniswap coin warning remover
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  
+// @version      0.2
+// @description
 // @author       github.com/bo0st3r
-// @match        https://app.uniswap.org/
+// @match        https://app.uniswap.org/*
+// @match        https://pancakeswap.finance/*
 // @grant        none
 // ==/UserScript==
 
 (async function() {
     'use strict';
 
+    console.log('Uniswap/Pancakeswap script loaded');
+
     function wait(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
-    let understandBtn, tokenDismissBtn, i=0;
+    let tokenDismissBtn, i=0;
     // Loops until buttons are found or reached max trials
-    while(!understandBtn && !tokenDismissBtn && i++ < 30){
+    while(!tokenDismissBtn && i++ < 30){
         await wait(200).then(() => {
-            console.log('try');
-            understandBtn = document.querySelector('.\\.understand-checkbox');
-            console.log(understandBtn);
             tokenDismissBtn = document.querySelector('.\\.token-dismiss-button');
         });
     }
 
-    // Click buttons
-    if(understandBtn && tokenDismissBtn){
-        understandBtn.click();
+    const iUnderstandBtn = document.getElementsByName('confirmed')[0];
+
+    // Click button
+    if(tokenDismissBtn){
+        if(iUnderstandBtn){
+          iUnderstandBtn.click();
+        }
+
         tokenDismissBtn.click();
     }
 })();
